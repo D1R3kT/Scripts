da33c177f3e4bece5a6aed84b61495ebb53c5d94
David_Yegorov <egor0vdavid@yandex.ru>
2025-10-31 12:11:51 +0300

    test commit1

--- RecoveryDocument_7e9dc13d-e264-4f68-9872-2f317ff95802.sh
+++ RecoveryDocument_7e9dc13d-e264-4f68-9872-2f317ff95802.sh
@@ -26,4 +26,4 @@ get_db() {
 }
 
 $docker_pfx psql --dbname $(get_db 'ekd_ekd') -c "SET search_path to public, ekd_ekd;
-UPDATE ekd_ekd.document SET deleted_date = NULL, deleter_id = NULL WHERE id = '12345';"
+UPDATE ekd_ekd.document SET deleted_date = NULL, deleter_id = NULL WHERE id = 'ec2c3206-3a9c-4f0d-99fb-e65137a25c7b';"
